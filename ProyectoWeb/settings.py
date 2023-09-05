19b3595bb1ad9cc43e611d53f9db46df7a8506d1
José Javier <70950479+Zeta997@users.noreply.github.com>
2023-09-05 10:34:03 +0200

    Update settings.py

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qqcs)(x5f6djh27z2$&j&q^@h$q6&t8!6l3h-)4@hucyzeu-hu'
+SECRET_KEY = '###########'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
